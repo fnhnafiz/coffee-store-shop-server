9eac331cd40a78e120a97cbe3398e6029c56ba52
fnhnafiz <nafsinsifan2k19@gmail.com>
2024-11-28 14:57:36 +0600

    update server side

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    client.connect();
 
     // create a folder and the name of folder "coffeeDB" and file name "coffee"
     const coffeeCollection = client.db("coffeeDB").collection("coffee");
