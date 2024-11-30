84b1d418166ce375baf7d872d227d83280379157
fnhnafiz <nafsinsifan2k19@gmail.com>
2024-11-30 18:27:58 +0600

    update get post patch fot auth

--- index.js
+++ index.js
@@ -30,6 +30,9 @@ async function run() {
     // create a folder and the name of folder "coffeeDB" and file name "coffee"
     const coffeeCollection = client.db("coffeeDB").collection("coffee");
 
+    // Create a Database for User
+    const userCollection = client.db("coffeeDB").collection("users");
+
     // show and convert object with an toArray with Read oparators
     app.get("/details", async (req, res) => {
       const cursor = coffeeCollection.find();
@@ -84,6 +87,51 @@ async function run() {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const result = await coffeeCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    // Create Register and Store User api in Database
+    // Create Register and Store User api in Database
+    // Create Register and Store User api in Database
+
+    // show the user data or access to the user data;
+
+    app.get("/users", async (req, res) => {
+      const cursor = userCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    // users Register data save to the DB in the first time;
+    app.post("/users", async (req, res) => {
+      const registerUser = req.body;
+
+      const result = await userCollection.insertOne(registerUser);
+      res.send(result);
+    });
+
+    // speciphic user data update with patch and using email
+
+    app.patch("/users/:email", async (req, res) => {
+      const email = req.params.email;
+      const filter = { email };
+      const updatedUser = {
+        $set: {
+          lastSignInTime: req?.body?.lastSignInTime,
+        },
+      };
+      const result = await userCollection.updateOne(filter, updatedUser);
+      res.send(result);
+    });
+
+    // User Delete from client side and database
+    // User Delete from client side and database
+    // User Delete from client side and database
+    app.delete("/users/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+
+      const result = await userCollection.deleteOne(query);
       res.send(result);
     });
 
